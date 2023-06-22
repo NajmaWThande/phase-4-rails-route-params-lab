92c4dd082a129404f6b44f49a439cb81a0136ffd
Najma <najmathande@gmail.com>
2023-06-22 03:04:18 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,15 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+      student = Student.find(params[:id])
+      render json: student
+  end
+  
+  def grades
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+  
+
 end
